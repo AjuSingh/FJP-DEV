01ec18ba13a0d6011838cbb9e8e12895e9974778
Ajvinder Singh <aju90534@gmail.com>
2022-02-20 00:03:49 +0530

    automatic conversions in js

---
+++ Javascript/AutomaticConv.js
@@ -0,0 +1,13 @@
+console.log("5" + 1);
+//51
+console.log("1" + 5);
+//15
+console.log("5" - 1);
+//4
+console.log("52" - 1 + 2);
+//53
+console.log(null+10); //null is act like zero here so output is 10
+console.log(undefined+10); //null is act like zero here so output is NaN
+//null and undefined are same but any calculation is done with undefined leads to result NaN
+//any unsensical calculation leads the result to NaN
+console.log("ten" - 4); //NaN
