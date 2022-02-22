4febbde788a2d1916ab5fccdfb4f643273711311
Ajvinder Singh <aju90534@gmail.com>
2022-02-23 00:00:07 +0530

    scope chainig and lexical enviornment

--- Javascript/Scopes.js
+++ Javascript/Scopes.js
@@ -9,5 +9,10 @@ var b = 10;
 outer();
 console.log(b);
 
+//scope is variable and function inside the scope of enviornment
+
 //output will be 10 10
 //because of scope chaining ans lexical enviornment
+
+//lexical enviornment is local memory of its own and local memory of its parent
+//scope chaining is chaining of lexical enviornment to access the variables
